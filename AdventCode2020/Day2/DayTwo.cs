fdf6e930ff27126ffffbf5f7a26a2a5027d28e2c
Michael Sakyiama <michael.sakyiama@veracityglobal.com>
2020-12-07 16:29:02 +0000

    Half way through part2

--- AdventCode2020/Day2/DayTwo.cs
+++ AdventCode2020/Day2/DayTwo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode2020.Day1
@@ -14,8 +16,30 @@ namespace AdventOfCode2020.Day1
         //Read the data, and store it in a dictionary. 
         //Then come up with an algorithm to compare each one key and value simultaneously
         //Check the rule and the password that follows after the colon. 
-        private static void ReadData() { }
-        public void Solution() { }
+        private static List<string> ReadData(string filePath) 
+        {
+            List<string> lines = File.ReadAllLines(filePath).ToList(); 
+            return lines;
+        }
+
+        private Dictionary<string,string> GroupData() 
+        {
+            string filePath = @"InputData\password_policies.txt";
+            List<string> DataList = ReadData(filePath);
+            Dictionary<string, string> PasswordRulePairs = new Dictionary<string, string>(); 
+            foreach (string line in DataList) 
+            {
+                string[] tempArray = line.Split(":");
+                PasswordRulePairs.Add(tempArray[0],tempArray[1]);
+            }
+            return PasswordRulePairs;
+        }
+
+        public void Solution() 
+        {
+            Dictionary<string, string> PwdAndRules = GroupData();
+            
+        }
 
     }
 }
