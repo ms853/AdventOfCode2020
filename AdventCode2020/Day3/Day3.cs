9ab5121c15ff6a58066cef490d4f6919991d0554
Michael Sakyiama <michael.sakyiama@veracityglobal.com>
2020-12-09 17:42:44 +0000

    Day3 in working progress

--- AdventCode2020/Day3/Day3.cs
+++ AdventCode2020/Day3/Day3.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace AdventOfCode2020.Day3
 {
-    class Day3
+    ///--- Day 3: Toboggan Trajectory ---
+    
+    class Day3 
     {
+        const string filePath = @"InputData\map_input";
+
+        private static List<string> ReadData(string filePath)
+        {
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            return lines;
+        }
+
+        public static void SolutionToPart1() 
+        {
+            List<string> MapData = ReadData(filePath);
+        }
+
+        public static void SolutionToPart2() { }
+
+        
     }
 }
