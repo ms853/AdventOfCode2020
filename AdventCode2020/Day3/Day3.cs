a009a495b9b716482792e45528ca2c89477a3471
Michael Sakyiama <michael.sakyiama@veracityglobal.com>
2020-12-11 15:42:14 +0000

    My solution to Day3 part2

--- AdventCode2020/Day3/Day3.cs
+++ AdventCode2020/Day3/Day3.cs
@@ -11,7 +11,6 @@ using System.Xml.Xsl;
 namespace AdventOfCode2020.Day3
 {
     ///--- Day 3: Toboggan Trajectory ---
-
     public class Day3
     {
         const string filePath = @"InputData\map_input.txt";
@@ -33,14 +32,14 @@ namespace AdventOfCode2020.Day3
             return numberOfTrees;
         }
 
+        //Part 1
         private static int TraverseMap(List<string> mapData)
         {
             
             int treesTotal = 0;
             int xSlope = 3;
             int x = 0;
-
-
+             
             foreach(string line in mapData) 
             {
                 if (x >= line.Length)
@@ -60,8 +59,56 @@ namespace AdventOfCode2020.Day3
             return treesTotal;
         }
 
-        public static void SolutionToPart2() { }
+        public static int SolutionToPart2() 
+        {
+            //string testFile = @"InputData\test.txt";
+            List<string> Map = ReadData(filePath);
+            int totalTreesPerSlope = TotalNoOfTreesEncountered(Map);
+            return totalTreesPerSlope;
+        }
 
-        
+        //Part 2
+        private static int TotalNoOfTreesEncountered(List<string> mapData) 
+        {
+            int slopeOne, slopeTwo, slopeThree, slopeFour, slopeFive;
+           
+            slopeOne = TraverseMapWithSlope(1, 1, mapData);
+            slopeTwo = TraverseMapWithSlope(3, 1, mapData);
+            slopeThree = TraverseMapWithSlope(5, 1, mapData);
+            slopeFour = TraverseMapWithSlope(7, 1, mapData);
+            slopeFive = TraverseMapWithSlope(1, 2, mapData);
+
+            return slopeOne * slopeTwo * slopeThree * slopeFour * slopeFive;
+            
+        }
+
+        private static int TraverseMapWithSlope(int xSlope, int ySlope, List<string> mapData)
+        {
+            int y = 0;
+            int x = 0;
+            int treesCounter = 0;
+            string[] mapArray = mapData.ToArray();
+
+            while (y < mapArray.Length) 
+            {
+                if (x >= mapArray[y].Length) 
+                {
+                    x -= mapArray[y].Length;
+                }
+                
+                if (mapArray[y][x] == '#') 
+                {
+                    treesCounter++;
+                }
+
+                x += xSlope;
+                y += ySlope;
+                
+            }
+
+            return treesCounter;
+        }
+
+
     }
 }
