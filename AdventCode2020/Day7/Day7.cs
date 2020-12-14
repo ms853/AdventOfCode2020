d6722b0756931bdcc3f65edf250bb75a90225870
Michael Sakyiama <michael.sakyiama@veracityglobal.com>
2020-12-14 01:51:49 +0000

    Day 7 incomplete but started.

---
+++ AdventCode2020/Day7/Day7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day7
+{
+    /// <summary>
+    /// --- Day 7: Handy Haversacks ---
+    /// </summary>
+    public class Day7
+    {
+        const string fileName = @"InputData\rules_input.txt";
+
+        private static string ReadData(string filePath)
+        {
+            string lines = null;
+            using (var sr = new StreamReader(filePath))
+            {
+                lines = sr.ReadToEnd();
+            }
+            return lines;
+        }
+    }
+}
