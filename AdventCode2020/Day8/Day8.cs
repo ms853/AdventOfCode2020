aed4830dfa5696ae4f013cc2e318e3255342ee20
Michael Sakyiama <michael.sakyiama@veracityglobal.com>
2020-12-23 17:12:11 +0000

    Day8 initial commit

---
+++ AdventCode2020/Day8/Day8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day8
+{
+    /// <summary>
+    /// --- Day 8: Handheld Halting ---
+    /// </summary>
+    public class Day8
+    {
+        const string fileName = @"InputData\boot_input.txt";
+        private static List<string> ReadData(string filePath)
+        {
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            return lines;
+        }
+
+        private static void ParseInput() 
+        {
+            
+        }
+
+        public static void Part1() 
+        {
+        
+        }
+    }
+}
